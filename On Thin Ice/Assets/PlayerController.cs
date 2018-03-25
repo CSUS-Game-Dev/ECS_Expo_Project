b8448c3f00bd8fce49d346895547d11e5e95677d
Bryce Collins <brycecollins0@gmail.com>
2018-03-25 13:49:57 -0700

    Sample track made

---
+++ On Thin Ice/Assets/PlayerController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public Rigidbody rigidbody;
+
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.UP)){
+			
+		}
+	}
+}
