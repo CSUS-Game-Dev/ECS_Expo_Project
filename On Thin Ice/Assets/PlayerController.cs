888d94c1cda911792c1776015cf0a2d397494d8a
NikSorvari <nik_sorvari@yahoo.com>
2018-03-25 18:13:43 -0700

    Player Controls

--- On Thin Ice/Assets/PlayerController.cs
+++ On Thin Ice/Assets/PlayerController.cs
@@ -5,12 +5,38 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
 
 	public Rigidbody rigidbody;
+	public SphereCollider sCol;
+	public GameObject Collider;
 
+	private float maxSpeed = .3f;
+	private float accelMagnitude = .01f;
+	private float speed = 0f;
+
+	private float Forward;
+	private float Turn;
+	private float Brake;
+
+	private Rigidbody rb;
+
+	void Start() {
+		rb = GetComponent<Rigidbody> ();
+	}
 	
 	// Update is called once per frame
 	void Update () {
-		if(Input.GetKeyDown(KeyCode.UP)){
-			
-		}
+		Forward = Input.GetAxis ("Vertical");
+		Turn = Input.GetAxis ("Horizontal");
+		Brake = Input.GetAxis ("Jump");
+
+
+		speed = speed + Forward * accelMagnitude; 
+
+		Quaternion q;
+		Vector3 v;
+		transform.Translate (0, 0, speed);
+		transform.Rotate (0, Turn * 6, 0);
+
+		speed = Mathf.Min (maxSpeed, speed);
+		speed = Mathf.Max (0f, speed - .001f);
 	}
 }
