3a405f62b07e86bbf0e4a7bafee5685387102051
DGinder <darrin.ginder@yahoo.com>
2018-03-30 19:01:01 -0700

    Main Menu Created

---
+++ On Thin Ice/Assets/MinusAction.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MinusAction : MonoBehaviour {
+
+    public Text text;
+
+    public void MinusText()
+    {
+        int temp = int.Parse(text.text);
+        if (temp > 1)
+        {
+            temp--;
+            text.text = temp.ToString();
+        }
+    }
+}
---
+++ On Thin Ice/Assets/PlusAction.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlusAction : MonoBehaviour {
+
+    public Text text;
+
+    public void PlusText()
+    {
+        int temp = int.Parse(text.text);
+        if(temp < 4)
+        {
+            temp++;
+            text.text = temp.ToString();
+        }
+    }
+}
